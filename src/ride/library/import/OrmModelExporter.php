3218773ee0a29fed51b81bf348dd622ad1e2c36a
Kaya <kayalion@kayalion.net>
2016-02-12 14:23:00 +0100

    added decorator support for all fields

--- src/ride/library/import/OrmModelExporter.php
+++ src/ride/library/import/OrmModelExporter.php
@@ -148,30 +148,31 @@ class OrmModelExporter {
         // map fields
         foreach ($fields as $fieldName => $field) {
             $columnName = $this->getColumnName($field);
-            if ($field instanceof RelationField) {
-                // custom decorators take priority
-                $decorator = $field->getOption('scaffold.export.decorator');
-                if ($decorator) {
-                    if (strpos($decorator, '#')) {
-                        list($interface, $id) = explode('#', $decorator, 2);
-                    } else {
-                        $interface = $decorator;
-                        $id = null;
-                    }
 
-                    $dependencyInjector = $model->getOrmManager()->getDependencyInjector();
-                    $decorator = $dependencyInjector->get($interface, $id);
-
-                    $destinationProvider->setColumnName($columnIndex, $columnName);
-
-                    $mapper->mapColumn($fieldName, $columnName);
-                    $mapper->addDecorator($columnName, $decorator);
-
-                    $columnIndex++;
-
-                    continue;
+            // custom decorators take priority
+            $decorator = $field->getOption('scaffold.export.decorator');
+            if ($decorator) {
+                if (strpos($decorator, '#')) {
+                    list($interface, $id) = explode('#', $decorator, 2);
+                } else {
+                    $interface = $decorator;
+                    $id = null;
                 }
 
+                $dependencyInjector = $model->getOrmManager()->getDependencyInjector();
+                $decorator = $dependencyInjector->get($interface, $id);
+
+                $destinationProvider->setColumnName($columnIndex, $columnName);
+
+                $mapper->mapColumn($fieldName, $columnName);
+                $mapper->addDecorator($columnName, $decorator);
+
+                $columnIndex++;
+
+                continue;
+            }
+
+            if ($field instanceof RelationField) {
                 // relation field on top level
                 $relationModelName = $field->getRelationModelName();
                 $relationModel = $orm->getModel($relationModelName);
